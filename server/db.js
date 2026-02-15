05729b7dd083dcbf97055ebe61ac696c0b91bedf
marc <marcandreinisperos13@gmail.com>
2026-02-16 03:57:52 +0800

    bug fix 1

--- server/db.js
+++ server/db.js
@@ -2,11 +2,10 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Neon
+  },
 });
 
 pool.connect((err) => {
