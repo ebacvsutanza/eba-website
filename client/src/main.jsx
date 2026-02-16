8d4f8c7dcf720341347a6186bd9ce0e84305d54f
marc <marcandreinisperos13@gmail.com>
2026-02-16 21:12:54 +0800

    bug testing 4

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+console.log(
+  "Using frontend Google Client ID:",
+  import.meta.env.VITE_GOOGLE_CLIENT_ID,
+);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
